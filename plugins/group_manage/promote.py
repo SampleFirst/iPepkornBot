39dcdb1a09e064355c3a5545b23b9e0c07e32d22
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-31 01:43:17 +0530

    Update promote.py

--- plugins/group_manage/promote.py
+++ plugins/group_manage/promote.py
@@ -3,8 +3,7 @@ from pyrogram.types import ChatPermissions
 from plugins.helper.admin_check import admin_check
 from plugins.helper.extract import extract_time, extract_user
 
-
-@Client.on_message(filters.group & filters.command("promote"))
+@Client.on_message((filters.group | filters.private) & filters.command("promote"))
 async def promote_user(_, message):
     is_admin = await admin_check(message)
     if not is_admin:
@@ -16,35 +15,35 @@ async def promote_user(_, message):
         await message.reply_text("You don't seem to be referring to a user.")
         return
 
-    user_member = await chat.get_member(user_id)
+    user_member = await message.chat.get_member(user_id)
     if user_member.status in ['administrator', 'creator']:
         await message.reply_text("The user is already an admin.")
         return
 
-    if user_id == bot.id:
+    if user_id == _.me.id:
         await message.reply_text("I can't promote myself!")
         return
         
-    bot_member = await chat.get_member(bot.id)
+    bot_member = await message.chat.get_member(_.me.id)
 
-    await bot.promote_chat_member(
-        chat.id, user_id,
-        can_change_info=bot_member.can_change_info,
-        can_post_messages=bot_member.can_post_messages,
-        can_edit_messages=bot_member.can_edit_messages,
-        can_delete_messages=bot_member.can_delete_messages,
-        can_invite_users=bot_member.can_invite_users,
-        can_restrict_members=bot_member.can_restrict_members,
-        can_pin_messages=bot_member.can_pin_messages,
-        can_promote_members=bot_member.can_promote_members
-    )
+    try:
+        await _.promote_chat_member(
+            message.chat.id, user_id,
+            can_change_info=bot_member.can_change_info,
+            can_post_messages=bot_member.can_post_messages,
+            can_edit_messages=bot_member.can_edit_messages,
+            can_delete_messages=bot_member.can_delete_messages,
+            can_invite_users=bot_member.can_invite_users,
+            can_restrict_members=bot_member.can_restrict_members,
+            can_pin_messages=bot_member.can_pin_messages,
+            can_promote_members=bot_member.can_promote_members
+        )
     except Exception as error:
         await message.reply_text(str(error))
     else:
         await message.reply_text(f"✨ {user_first_name} has been promoted to an admin! 🎉")
 
-        
-@Client.on_message(filters.group & filters.command("demote"))
+@Client.on_message((filters.group | filters.private) & filters.command("demote"))
 async def demote_user(_, message):
     is_admin = await admin_check(message)
     if not is_admin:
@@ -56,7 +55,7 @@ async def demote_user(_, message):
         await message.reply_text("You don't seem to be referring to a user.")
         return
 
-    user_member = await chat.get_member(user_id)
+    user_member = await message.chat.get_member(user_id)
     if user_member.status == 'creator':
         await message.reply_text("Cannot demote the chat creator.")
         return
@@ -65,13 +64,13 @@ async def demote_user(_, message):
         await message.reply_text("The user is not an admin.")
         return
 
-    if user_id == bot.id:
+    if user_id == _.me.id:
         await message.reply_text("I can't demote myself!")
         return
 
     try:
-        await bot.promote_chat_member(
-            chat.id,
+        await _.promote_chat_member(
+            message.chat.id,
             user_id,
             can_change_info=False,
             can_post_messages=False,
@@ -83,7 +82,7 @@ async def demote_user(_, message):
             can_promote_members=False
         )
 
-        await chat.restrict_member(user_id, ChatPermissions(can_send_messages=True))  # Assuming this is the desired restriction
+        await message.chat.restrict_member(user_id, ChatPermissions(can_send_messages=True))
     except Exception as error:
         await message.reply_text(str(error))
     else:
