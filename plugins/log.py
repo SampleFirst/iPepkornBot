bb0c5169a9369d6e5a3c1c5cf7422c22baf9f951
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-30 13:00:08 +0530

    Update log.py

--- plugins/log.py
+++ plugins/log.py
@@ -5,35 +5,24 @@ import datetime
 import pytz
 from info import ADMINS, LOG_CHANNEL
 
-# Convert UTC time to Indian Timezone
-def get_indian_time(hour=0, minute=0):
-    utc_now = datetime.datetime.now(pytz.utc)
-    indian_timezone = pytz.timezone("Asia/Kolkata")
-    return utc_now.replace(hour=hour, minute=minute).astimezone(indian_timezone)
-
-
 # Initialize a list to keep track of the current digit values
 current_digits = [0, 0, 0, 0]
 
 # Dictionary to keep track of active tasks
 active_tasks = {}
 
-# Modified send_log_message function
-async def send_log_message(client, chat_id, hour, minute):
+# Add AM/PM toggle button state
+am_pm_toggle = 0  # 0 for AM, 1 for PM
+
+# Function to send log message
+async def send_log_message(chat_id, new_hour, new_minute):
     while True:
-        current_time = datetime.datetime.now(pytz.utc)
-        indian_now = get_indian_time()
-
-        if indian_now.hour == hour and indian_now.minute == minute:
-            message = f"This is a daily log message. Current Indian Time: {indian_now.strftime('%Y-%m-%d %H:%M')}"
-            await self.send_message(chat_id=LOG_CHANNEL, message)
-            
-        await asyncio.sleep(10)  # Sleep for 1 minute
-        
-        # Check if the task needs to be stopped
-        if chat_id not in active_tasks:
-            break
-
+        now = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+        current_hour = now.hour if am_pm_toggle == 0 else (now.hour + 12) % 24
+        if current_hour == new_hour and now.minute == new_minute:
+            message_text = f"Reporting time has arrived for user {chat_id}."
+            await bot.send_message(LOG_CHANNEL, message_text)
+        await asyncio.sleep(60)  # Check every minute
 
 # Add command handler and callback handler
 @Client.on_message(filters.command('Report') & filters.user(ADMINS))
@@ -109,7 +98,8 @@ async def callback_handler(_, query: CallbackQuery):
             await query.message.edit_text("Reporting stopped.", reply_markup=keyboard)
     
     elif data == "change_time":
-        # Create toggle buttons for each digit, showing their current values
+        global am_pm_toggle
+        # Create toggle buttons for each digit and AM/PM toggle
         keyboard = InlineKeyboardMarkup(
             [
                 [
@@ -119,13 +109,16 @@ async def callback_handler(_, query: CallbackQuery):
                     InlineKeyboardButton(str(current_digits[3]), callback_data="set_digit_3"),
                 ],
                 [
+                    InlineKeyboardButton("AM" if am_pm_toggle == 0 else "PM", callback_data="toggle_am_pm"),
+                ],
+                [
                     InlineKeyboardButton("Set Time", callback_data="confirm_time"),
                     InlineKeyboardButton("Cancel", callback_data="cancel_log"),
                 ]
             ]
         )
         await query.message.edit_text("Please select the new time in HH:MM format.", reply_markup=keyboard)
-    
+        
     elif data.startswith("set_digit_"):
         digit_index = int(data.split("_")[2])
         current_digit = current_digits[digit_index]
@@ -138,17 +131,24 @@ async def callback_handler(_, query: CallbackQuery):
         keyboard = query.message.reply_markup.inline_keyboard
         keyboard[0][digit_index].text = str(new_digit)
         await query.message.edit_reply_markup(InlineKeyboardMarkup(inline_keyboard=keyboard))
-
+    
+    elif data == "toggle_am_pm":
+        am_pm_toggle = 1 - am_pm_toggle  # Toggle between AM and PM
+        keyboard = query.message.reply_markup.inline_keyboard
+        keyboard[1][0].text = "AM" if am_pm_toggle == 0 else "PM"
+        await query.message.edit_reply_markup(InlineKeyboardMarkup(inline_keyboard=keyboard))
+        
     elif data == "confirm_time":
         new_hour = current_digits[1] * 10 + current_digits[0]
         new_minute = current_digits[3] * 10 + current_digits[2]
-
+        
         if 0 <= new_hour < 24 and 0 <= new_minute < 60:
             if chat_id in active_tasks:
                 active_tasks[chat_id].cancel()
                 del active_tasks[chat_id]
             active_tasks[chat_id] = asyncio.create_task(send_log_message(chat_id, new_hour, new_minute))
-            status_text = f"Reporting time changed. Reporting started at {new_hour:02d}:{new_minute:02d}."
+            am_pm_text = "AM" if am_pm_toggle == 0 else "PM"
+            status_text = f"Reporting time changed. Reporting started at {new_hour:02d}:{new_minute:02d} {am_pm_text}."
         else:
             status_text = "Invalid time format. Please enter a valid time."
 
@@ -169,4 +169,4 @@ async def callback_handler(_, query: CallbackQuery):
         await query.message.edit_text("Action canceled.")
         await asyncio.sleep(5)
         await query.message.delete()
-    
+        
