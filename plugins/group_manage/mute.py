0d2f5a0c9a9bcfd5131e5ff9ceea20d22b36b9af
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-31 02:17:02 +0530

    Update mute.py

--- plugins/group_manage/mute.py
+++ plugins/group_manage/mute.py
@@ -87,9 +87,48 @@ async def temp_mute_user(_, message):
                 " Mouth "
                 f" muted for {message.command[1]}!"
             )
+            
+@Client.on_message(filters.command("promote"))
+async def promote_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        await message.reply_text(
+            "Attention: Admin Privileges Required\n\n"
+            "Dear member,\n\n"
+            "However, to access this, we kindly request that you ensure you have admin privileges within our group."
+        )
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    try:
+        await message.chat.promote_member(user_id)
+    except Exception as error:
+        await message.reply_text(str(error))
+    else:
+        await message.reply_text(
+            f"✨ {user_first_name} has been promoted to an admin! 🎉"
+        )
 
 
+@Client.on_message(filters.command("demote"))
+async def demote_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        await message.reply_text(
+            "Attention: Admin Privileges Required\n\n"
+            "Dear member,\n\n"
+            "To access this, we kindly request that you ensure you have admin privileges within our group."
+        )
+        return
 
+    user_id, user_first_name = extract_user(message)
 
-
-
+    try:
+        await message.chat.restrict_member(user_id, ChatPermissions())
+    except Exception as error:
+        await message.reply_text(str(error))
+    else:
+        await message.reply_text(
+            f"🔥 {user_first_name} has been demoted to a regular member!"
+        )
