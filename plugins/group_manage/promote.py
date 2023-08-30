dc6a1210b4eca98d0af5a789f5acc68f1babff08
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-31 02:05:03 +0530

    Update promote.py

--- plugins/group_manage/promote.py
+++ plugins/group_manage/promote.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters, Message
+from pyrogram import Client, filters
 from pyrogram.types import ChatPermissions
 from plugins.helper.admin_check import admin_check
 from plugins.helper.extract import extract_user
